0ac811b8054914082a46f58af2c5ac394d8ec5f2
Sergio <sergio.n.m.jr@gmail.com>
2023-02-27 16:28:41 -0300

    enviando dotenv

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 import {connection} from "./db/connections.js";
+
 import app from '../app.js'
-const PORT = 3000
 
 
-app.listen(PORT, async()=>{
-    console.log(`Executando na porta ${PORT}`);
+app.listen(process.env.PORT, async()=>{
+    console.log(`Executando na porta ${process.env.PORT}`);
     /* const [result] = await connection.execute("SELECT 1")
         if (result){
         console.log("funcionou")
