9aeb8b44430db069cf822b09a38cb1104634ed1b
Sergio <sergio.n.m.jr@gmail.com>
2023-02-27 17:41:19 -0300

    pasta frontend

--- front-end/envia.requisicao.login.js
+++ front-end/envia.requisicao.login.js
@@ -3,8 +3,8 @@
 async function enviaRequisicaoLogin(){
     inputUsername=document.querySelector("#username").value
     inputPassword=document.querySelector("#password").value
-        res = await axios.get('http://127.0.0.1:3000/login');
-        console.log(res.data.token)
+        res = await axios.get('https://backend-express-production-0fa9.up.railway.app/login');
+        res.data.token
     
 }
 
