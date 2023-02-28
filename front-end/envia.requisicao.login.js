98411becaee064f858f1a3af78ecde5a0f9659f4
Sergio <sergio.n.m.jr@gmail.com>
2023-02-27 22:33:02 -0300

    ttttt

--- front-end/envia.requisicao.login.js
+++ front-end/envia.requisicao.login.js
@@ -1,12 +1,13 @@
-
-
-async function enviaRequisicaoLogin(){
-    inputUsername=document.querySelector("#username").value
-    inputPassword=document.querySelector("#password").value
-        res = await axios.get('https://backend-express-production-0fa9.up.railway.app/login');
-        console.log(res.data.token)
-    
+async function enviaRequisicaoLogin() {
+  inputUsername = document.querySelector("#username").value;
+  inputPassword = document.querySelector("#password").value;
+  res = axios({
+    method: "GET",
+    url: "https://backend-express-production-0fa9.up.railway.app/login",
+    data: {
+      user: inputUsername,
+      password: inputPassword,
+    },
+  });
+  console.log(res.data.token);
 }
-
-
-
